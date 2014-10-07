0789cc03e97abf843ff3a63b1dacfe437eab8219
Décio Moritz <deciomoritz@gmail.com>
2014-10-07 02:08:22 -0300

    Inimigos perseguindo 1 soldado, câmera isométrica implementada e 1 soldado se movendo via teclado(para testes)

---
+++ Assets/scripts/FollowSoldier.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowSoldier : MonoBehaviour {
+
+
+	public GameObject soldier;
+	public float speed;
+
+	void Start () {
+	}
+
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, soldier.transform.position, speed*Time.deltaTime);
+	}
+}
